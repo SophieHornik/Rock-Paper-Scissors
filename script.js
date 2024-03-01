b05e36168edece6051509740bb44d1df3945c022
SophieHornik <144719454+SophieHornik@users.noreply.github.com>
2024-03-01 18:53:29 +0000

    got username

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-
+let username = prompt('Please enter your username.')
+document.getElementById('Userch').innerHTML = `<h1 class="usertext">${username}'s <br> Choice</h1>`
+document.getElementById('userScoreDisplay').textContent = `${username}'s Score:`
