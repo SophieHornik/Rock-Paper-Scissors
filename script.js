db1ee52412553d033e16b11f973333a7092596fa
SophieHornik <144719454+SophieHornik@users.noreply.github.com>
2024-03-02 19:19:55 +0000

    got computer choice and  added onclicks for buttons (I think please send help)

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 let username = prompt('Please enter your username.')
 document.getElementById('Userch').innerHTML = `<h1 class="usertext">${username}'s <br> Choice</h1>`
 document.getElementById('userScoreDisplay').textContent = `${username}'s Score:`
+
+function playGame(userChoice){
+//Get computer choice
+let randomNumber = math.Floor(Math.random() * 3);
+let compChoice
+if (randomNumber === 0){
+    compChoice="rock"
+} else if (randomNumber === 1 ){
+    compChoice = "paper"
+}  else if (randomNumber === 2 ){
+        compChoice = "scissors"}
+}
