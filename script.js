eb8886a66f60ce527b23b4e513ba0b03c902851a
SophieHornik <144719454+SophieHornik@users.noreply.github.com>
2024-03-11 01:57:09 +0000

    attempted js and fixed title

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let username = prompt('Please enter your username.')
 document.getElementById('Userch').innerHTML = `<h1 class="usertext">${username}'s <br> Choice</h1> <img id="userpic" src="usericonn (1).png" alt="..." />`
 document.getElementById('userScoreDisplay').textContent = `${username}'s Score:`
-let userPic = document.getElementById('userPic');
 
 function playGame(userChoice){
 //Get computer choice
@@ -13,6 +12,7 @@ if (randomNumber === 0){
     compChoice = "paper"
 }  else if (randomNumber === 2 ){
         compChoice = "scissors"}
+
+//display user choice
 }
 
-//display
